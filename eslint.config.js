cac17357d88446a7b8f07e5df208d742ec1fdfd6
YoimelDev <yoimeldev@gmail.com>
2024-08-13 09:48:06 -0400

    fix: indent errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,31 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
 
 export default [
-    { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
+    { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
     { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
-    ...pluginVue.configs["flat/strongly-recommended"],
+    ...pluginVue.configs['flat/strongly-recommended'],
     {
-        files: ["**/*.{js,ts,vue}"],
+        plugins: {
+            'typescript-eslint': tseslint.plugin,
+        },
+        files: ['**/*.{js,ts,vue}'],
         languageOptions: { parserOptions: { parser: tseslint.parser } },
         rules: {
-            "vue/multi-word-component-names": "off",
-            "no-undef": "off",
+            'vue/multi-word-component-names': 'off',
+            'vue/html-indent': ['error', 4],
+            'no-undef': 'off',
+            'semi': ['error', 'never'],
+            'indent': ['error', 4], 
+            'quotes': ['error', 'single'],
+            'comma-dangle': ['error', 'always-multiline'],
         },
     },
-];
+    {
+        ignores: ['node_modules', 'vendor', 'dist', 'public'],
+    },
+]
