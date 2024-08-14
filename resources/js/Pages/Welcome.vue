c7f7a1218c3bfd84b8d0097bde08754a18ea65d3
YoimelDev <yoimeldev@gmail.com>
2024-08-14 08:05:48 -0400

    refactor: Remove unnecessary code from Welcome.vue

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -40,14 +40,6 @@ defineProps<{
                     </ul>
 
                     <nav class="flex gap-4">
-                        <Link
-                            v-if="$page.props.auth.user"
-                            :href="route('dashboard')"
-                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                        >
-                            Dashboard
-                        </Link>
-
                         <Button as-child>
                             <Link :href="route('login')">
                                 Inicia sesión
