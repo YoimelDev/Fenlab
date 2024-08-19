e054ddbddc21c2c7f015f034d194522f1c8c9504
YoimelDev <yoimeldev@gmail.com>
2024-08-19 07:54:34 -0400

    chore: Update Register.vue with new UI components and improve password visibility toggle button accessibility

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import GuestLayout from '@/Layouts/GuestLayout.vue'
 import InputError from '@/Components/InputError.vue'
-import InputLabel from '@/Components/InputLabel.vue'
-import PrimaryButton from '@/Components/PrimaryButton.vue'
-import TextInput from '@/Components/TextInput.vue'
 import { Head, Link, useForm } from '@inertiajs/vue3'
+
+import { EyeClosedIcon, EyeOpenIcon } from '@radix-icons/vue'
+import { Input } from '@/Components/ui/input'
+import { Label } from '@/Components/ui/label'
+import { Button } from '@/Components/ui/button'
+
+const showPassword = ref(false)
 
 const form = useForm({
     name: '',
@@ -28,15 +33,13 @@ const submit = () => {
 
         <form @submit.prevent="submit">
             <div>
-                <InputLabel
-                    for="name"
-                    value="Name"
-                />
+                <Label for="name">Name</Label>
 
-                <TextInput
+                <Input
                     id="name"
                     type="text"
-                    class="mt-1 block w-full"
+                    placeholder="Name"
+                    class="mt-2"
                     v-model="form.name"
                     required
                     autofocus
@@ -44,89 +47,125 @@ const submit = () => {
                 />
 
                 <InputError
-                    class="mt-2"
+                    class="mt-2 ml-4"
                     :message="form.errors.name"
                 />
             </div>
 
             <div class="mt-4">
-                <InputLabel
-                    for="email"
-                    value="Email"
-                />
+                <Label for="email">Email</Label>
 
-                <TextInput
+                <Input
                     id="email"
                     type="email"
-                    class="mt-1 block w-full"
+                    placeholder="Email"
+                    class="mt-2"
                     v-model="form.email"
                     required
+                    autofocus
                     autocomplete="username"
                 />
 
                 <InputError
-                    class="mt-2"
+                    class="mt-2 ml-4"
                     :message="form.errors.email"
                 />
             </div>
 
             <div class="mt-4">
-                <InputLabel
-                    for="password"
-                    value="Password"
-                />
+                <Label for="password">Contraseña</Label>
 
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="new-password"
-                />
+                <div class="relative w-full items-center">
+                    <Input
+                        id="password"
+                        :type="showPassword ? 'text' : 'password'"
+                        placeholder="Contraseña"
+                        class="mt-2"
+                        v-model="form.password"
+                        required
+                        autocomplete="new-password"
+                    />
+
+                    <button
+                        type="button"
+                        class="absolute end-0 inset-y-0 flex items-center justify-center px-2"
+                    >
+                        <EyeOpenIcon
+                            v-if="showPassword"
+                            class="w-5 h-5 text-[#697077] cursor-pointer"
+                            @click="showPassword = false"
+                        />
+
+                        <EyeClosedIcon
+                            v-else
+                            class="w-5 h-5 text-[#697077] cursor-pointer"
+                            @click="showPassword = true"
+                        />
+                    </button>
+                </div>
+
+                <span class="text-[#697077] text-xs leading-4">
+                    Debe ser una combinación de mínimo 8 letras, números y símbolos.
+                </span>
 
                 <InputError
-                    class="mt-2"
+                    class="mt-2 ml-4"
                     :message="form.errors.password"
                 />
             </div>
 
             <div class="mt-4">
-                <InputLabel
-                    for="password_confirmation"
-                    value="Confirm Password"
-                />
+                <Label for="password_confirmation">Confirmar Contraseña</Label>
 
-                <TextInput
-                    id="password_confirmation"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password_confirmation"
-                    required
-                    autocomplete="new-password"
-                />
+                <div class="relative w-full items-center">
+                    <Input
+                        id="password_confirmation"
+                        :type="showPassword ? 'text' : 'password'"
+                        placeholder="Confirmar contraseña"
+                        class="mt-2"
+                        v-model="form.password_confirmation"
+                        required
+                        autocomplete="new-password"
+                    />
+
+                    <button
+                        type="button"
+                        class="absolute end-0 inset-y-0 flex items-center justify-center px-2"
+                    >
+                        <EyeOpenIcon
+                            v-if="showPassword"
+                            class="w-5 h-5 text-[#697077] cursor-pointer"
+                            @click="showPassword = false"
+                        />
+
+                        <EyeClosedIcon
+                            v-else
+                            class="w-5 h-5 text-[#697077] cursor-pointer"
+                            @click="showPassword = true"
+                        />
+                    </button>
+                </div>
 
                 <InputError
-                    class="mt-2"
+                    class="mt-2 ml-4"
                     :message="form.errors.password_confirmation"
                 />
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-end gap-4 mt-4">
                 <Link
                     :href="route('login')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    class="text-sm text-[#001D6C] hover:text-gray-900 leading-5 rounded-md hover:underline"
                 >
                     Already registered?
                 </Link>
 
-                <PrimaryButton
-                    class="ms-4"
+                <Button
                     :class="{ 'opacity-25': form.processing }"
                     :disabled="form.processing"
                 >
                     Register
-                </PrimaryButton>
+                </Button>
             </div>
         </form>
     </GuestLayout>
