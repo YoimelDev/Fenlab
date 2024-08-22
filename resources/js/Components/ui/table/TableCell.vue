356f6b3749b99174aa0c780ea4612d2a60a766b9
YoimelDev <yoimeldev@gmail.com>
2024-08-22 09:37:48 -0400

    refactor: Update Table components for consistent formatting

--- resources/js/Components/ui/table/TableCell.vue
+++ resources/js/Components/ui/table/TableCell.vue
@@ -8,14 +8,14 @@ const props = defineProps<{
 </script>
 
 <template>
-  <td
-    :class="
-      cn(
-        'p-2 align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-0.5',
-        props.class,
-      )
-    "
-  >
-    <slot />
-  </td>
+    <td
+        :class="
+            cn(
+                'py-3 align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-0.5',
+                props.class,
+            )
+        "
+    >
+        <slot />
+    </td>
 </template>
