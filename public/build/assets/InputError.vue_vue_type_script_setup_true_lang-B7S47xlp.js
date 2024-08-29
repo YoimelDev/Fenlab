d226e1f218663078676dd763bf04364a757e42ef
YoimelDev <yoimeldev@gmail.com>
2024-08-29 11:50:13 -0400

    build

--- public/build/assets/InputError.vue_vue_type_script_setup_true_lang-B7S47xlp.js
+++
@@ -1,1 +0,0 @@
-import{d as s,l as t,x as a,o,g as r,b as n,t as p}from"./app-6sGTN3IA.js";const c={class:"text-sm text-red-600"},l=s({__name:"InputError",props:{message:{}},setup(i){return(e,m)=>t((o(),r("div",null,[n("p",c,p(e.message),1)],512)),[[a,e.message]])}});export{l as _};
