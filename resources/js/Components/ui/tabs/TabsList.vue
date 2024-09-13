69ea295cf647d52b50c7557b4d92b98a31a7eea3
YoimelDev <yoimeldev@gmail.com>
2024-09-13 04:21:34 -0400

    feat: Add Tabs component and related files

---
+++ resources/js/Components/ui/tabs/TabsList.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { type HTMLAttributes, computed } from 'vue'
+import { TabsList, type TabsListProps } from 'radix-vue'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<TabsListProps & { class?: HTMLAttributes['class'] }>()
+
+const delegatedProps = computed(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { class: _, ...delegated } = props
+
+    return delegated
+})
+</script>
+
+<template>
+    <TabsList
+        v-bind="delegatedProps"
+        :class="cn(
+            'inline-flex items-center justify-center p-0 text-muted-foreground bg-transparent border-b border-grey',
+            props.class,
+        )"
+    >
+        <slot />
+    </TabsList>
+</template>
---
+++ resources/js/Components/ui/tabs/TabsTrigger.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { type HTMLAttributes, computed } from 'vue'
+import { TabsTrigger, type TabsTriggerProps, useForwardProps } from 'radix-vue'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<TabsTriggerProps & { class?: HTMLAttributes['class'] }>()
+
+const delegatedProps = computed(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { class: _, ...delegated } = props
+
+    return delegated
+})
+
+const forwardedProps = useForwardProps(delegatedProps)
+</script>
+
+<template>
+    <TabsTrigger
+        v-bind="forwardedProps"
+        :class="cn(
+            'inline-flex items-center justify-center whitespace-nowrap px-3 py-1 text-sm text-[#686868] font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-electric-green focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border-b-2 border-b-transparent data-[state=active]:border-b-2 data-[state=active]:border-electric-green data-[state=active]:text-black',
+            props.class,
+        )"
+    >
+        <span class="truncate">
+            <slot />
+        </span>
+    </TabsTrigger>
+</template>
