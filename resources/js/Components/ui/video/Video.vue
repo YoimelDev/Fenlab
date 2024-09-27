731353a2d520a6e25021b75d266742966ff54418
YoimelDev <yoimeldev@gmail.com>
2024-09-27 06:14:46 -0400

    feat: Add Video component to UI module

---
+++ resources/js/Components/ui/video/Video.vue
@@ -0,0 +1,32 @@
+v-
+<script setup>
+import { onMounted } from 'vue'
+import Plyr from 'plyr'
+import 'plyr/dist/plyr.css'
+
+defineProps({
+    video: {
+        type: String,
+        required: true,
+    },
+})
+
+onMounted(() => {
+    new Plyr('#player')
+})
+</script>
+
+<template>
+    <div
+        id="player"
+        class="plyr__video-embed"
+        style="height: 100%; --plyr-color-main: #367E43"
+    >
+        <iframe
+            :src="video"
+            allowfullscreen
+            allowtransparency
+            allow="autoplay"
+        />
+    </div>
+</template>
