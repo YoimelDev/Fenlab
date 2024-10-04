fb2c238c9e6545357c71d341b940a5bdf2883228
YoimelDev <yoimeldev@gmail.com>
2024-10-03 23:44:32 -0400

    refactor: Update stroke color in DownloadIcon component

--- resources/js/Components/ui/button/index.ts
+++ resources/js/Components/ui/button/index.ts
@@ -12,7 +12,7 @@ export const buttonVariants = cva(
                 destructive:
           'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
                 outline:
-          'border-2 border-input text-primary bg-background shadow-sm hover:bg-accent',
+          'border-2 border-electric-green text-electric-green bg-background shadow-sm hover:bg-accent',
                 secondary:
           'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
                 ghost: 'hover:bg-[#004DF21A]',
