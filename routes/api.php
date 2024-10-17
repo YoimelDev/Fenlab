f72b33b4b63ffcc254f0ab293e639055133f9170
JandryNexia <jandry@nexia.io>
2024-10-17 15:34:36 +0200

    fix

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Auth\CreatePasswordController;
+use App\Http\Middleware\ApiKeyMiddleware;
 
-
-Route::middleware(['check_api_key'])->group(function () {
+Route::middleware([ApiKeyMiddleware::class])->group(function () {
   Route::post('/send-create-password', [CreatePasswordController::class, 'sendCreatePasswordEmail']);
   Route::post('/create-password', [CreatePasswordController::class, 'createPassword']);
 });
