463785c14dd1b98db80c10660ae3ef7d328d722d
YoimelDev <yoimeldev@gmail.com>
2024-10-23 10:45:44 -0400

    Refactor: Add SalesforceUser interface to types/index.d.ts

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -5,6 +5,17 @@ export interface User {
     email_verified_at: string
 }
 
+export interface SalesforceUser {
+    title?: string
+    rols: string
+    companyCommercialName: string
+    companyBusinessName?: string
+    companyId: string
+    email: string
+    name: string
+    id: string
+}
+
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
     auth: {
         user: User
