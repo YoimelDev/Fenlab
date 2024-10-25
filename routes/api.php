68544f60c694815d9dc6ab99e2d92068c3337e2f
YoimelDev <yoimeldev@gmail.com>
2024-10-25 08:16:43 -0400

    Refactor: Update middleware in api.php

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,12 @@ use App\Http\Controllers\Auth\CreatePasswordController;
 use App\Http\Controllers\FenlabApiController;
 use App\Http\Middleware\ApiKeyMiddleware;
 
-Route::middleware([ApiKeyMiddleware::class, 'web'])->group(function () {
+Route::middleware([ApiKeyMiddleware::class])->group(function () {
   Route::post('/send-create-password', [CreatePasswordController::class, 'sendCreatePasswordEmail']);
   Route::post('/create-password', [CreatePasswordController::class, 'createPassword'])->name('create-password');
+});
 
+Route::middleware([ApiKeyMiddleware::class, 'web'])->group(function () {
   Route::post('/', [FenlabApiController::class, 'store'])->name('api');
 });
+
