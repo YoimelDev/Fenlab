463e897c18e6820a65ba347f2e8fed8f5dd3cb90
YoimelDev <yoimeldev@gmail.com>
2025-01-10 06:57:38 -0400

    style: format Select components for improved readability and consistency

--- resources/js/Components/ui/select/SelectSeparator.vue
+++ resources/js/Components/ui/select/SelectSeparator.vue
@@ -6,12 +6,15 @@ import { computed, type HTMLAttributes } from 'vue'
 const props = defineProps<SelectSeparatorProps & { class?: HTMLAttributes['class'] }>()
 
 const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props
+    const { class: _, ...delegated } = props
 
-  return delegated
+    return delegated
 })
 </script>
 
 <template>
-  <SelectSeparator v-bind="delegatedProps" :class="cn('-mx-1 my-1 h-px bg-muted', props.class)" />
+    <SelectSeparator
+        v-bind="delegatedProps"
+        :class="cn('-mx-1 my-1 h-px bg-muted', props.class)"
+    />
 </template>
