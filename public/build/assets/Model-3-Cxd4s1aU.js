5316b0280314facde090087558f35675709c8eb8
YoimelDev <yoimeldev@gmail.com>
2025-01-13 09:39:26 -0400

    'build'

--- public/build/assets/Model-3-Cxd4s1aU.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./Model-3.vue_vue_type_script_setup_true_lang-D3EeTQdt.js";import"./plyr-C9HLHEGN.js";import"./app-DQolWMUO.js";import"./AccordionTrigger.vue_vue_type_script_setup_true_lang-C_iLyqgC.js";import"./utils-i-HCj8B3.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";export{o as default};
