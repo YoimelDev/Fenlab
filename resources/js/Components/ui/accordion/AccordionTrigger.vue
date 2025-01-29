a992907874a734556729deba810f9e114e8009a8
YoimelDev <yoimeldev@gmail.com>
2025-01-29 11:15:36 -0400

    feat: update AccordionTrigger component to fix SVG visibility states

--- resources/js/Components/ui/accordion/AccordionTrigger.vue
+++ resources/js/Components/ui/accordion/AccordionTrigger.vue
@@ -24,7 +24,7 @@ const delegatedProps = computed(() => {
             v-bind="delegatedProps"
             :class="
                 cn(
-                    'flex flex-1 items-center justify-between p-4 text-[#21272A] text-xl transition-all hover:underline [&[data-state=open]>svg]:rotate-180 [&[data-state=open]>svg:nth-child(1)]:hidden [&[data-state=closed]>svg:nth-child(2)]:hidden',
+                    'flex flex-1 items-center justify-between p-4 text-[#21272A] text-xl transition-all hover:underline [&[data-state=open]>svg]:rotate-180 [&[data-state=open]>svg:nth-child(2)]:hidden [&[data-state=closed]>svg:nth-child(3)]:hidden',
                     props.class,
                 )
             "
