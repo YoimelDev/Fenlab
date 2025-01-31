cb90e2c44f324bf478995c7b494947abc0f97d61
YoimelDev <yoimeldev@gmail.com>
2025-01-31 09:05:56 -0400

    feat: update contact email handling to support multiple recipients from environment variable

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -33,8 +33,8 @@ class ContactController extends Controller
      */
     public function store(StoreContactRequest $request)
     {
-
-        Mail::to("david.roig@fencia.es")->send(new ContactEmail());
+        $emails = explode(',', env('CONTACT_EMAILS'));
+        Mail::to($emails)->send(new ContactEmail());
 
         return response()->json(['message' => 'Contact created successfully'], 201);
     }
