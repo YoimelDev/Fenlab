e80fc0efc2d6441c34baac497d3f455f27e5b411
YoimelDev <yoimeldev@gmail.com>
2025-02-26 03:13:21 -0400

    fix: improve input handling and formatting for currency directive

--- resources/js/lib/directives.ts
+++ resources/js/lib/directives.ts
@@ -13,12 +13,16 @@ const needsConversion = (value: string): boolean => {
     return num < 10000 // Si es menor a 10000, asumimos que son céntimos
 }
 
-// Separar handlers
 function handleInput(e: InputEvent, el: HTMLInputElement) {
     if (e.inputType === 'insertText' || e.inputType === 'deleteContentBackward') {
-        const currentValue = el.value.replace(/[^\d.]/g, '')
+        const cursorPos = el.selectionStart || 0
+
+        const currentValue = el.value.replace(/[^0-9.]/g, '')
+
         el.dataset.rawValue = currentValue
         el.value = currentValue
+
+        el.setSelectionRange(cursorPos, cursorPos)
     }
 }
 
@@ -59,15 +63,17 @@ export const currencyDirective = {
         el.addEventListener('focus', elExt.__currencyDirectiveHandlers.focus)
     },
     updated(el: HTMLInputElement) {
-        const rawValue = el.value.replace(/[^\d.]/g, '')
-        el.dataset.rawValue = rawValue
-        let value = rawValue || '0'
-        if (!rawValue.includes('.')) {
-            if (needsConversion(rawValue)) {
-                value = (parseInt(rawValue, 10) / 100).toString()
+        if (!el.matches(':focus')) {
+            const rawValue = el.value.replace(/[^0-9.]/g, '')
+            el.dataset.rawValue = rawValue
+            let value = rawValue || '0'
+            if (!rawValue.includes('.')) {
+                if (needsConversion(rawValue)) {
+                    value = (parseInt(rawValue, 10) / 100).toString()
+                }
             }
+            el.value = formatCurrency(parseFloat(value))
         }
-        el.value = formatCurrency(parseFloat(value))
     },
     unmounted(el: HTMLInputElement) {
         const elExt = el as CurrencyDirectiveElement
