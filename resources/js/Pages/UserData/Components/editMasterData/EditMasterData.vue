24d83c8e813658ec8b2433697041b9e64d2fa29e
YoimelDev <yoimeldev@gmail.com>
2025-03-20 08:28:32 -0400

    feat: implement tabbed interface for editing macro and broker fee data

--- resources/js/Pages/UserData/Components/editMasterData/EditMasterData.vue
+++ resources/js/Pages/UserData/Components/editMasterData/EditMasterData.vue
@@ -18,6 +18,10 @@ import {
     TableHeader,
     TableRow,
     toast,
+    Tabs,
+    TabsList,
+    TabsTrigger,
+    TabsContent,
 } from '@/Components/ui'
 
 import { CompanyMasterData } from '@/types/fenlab'
@@ -78,98 +82,113 @@ const postData = async () => {
             </DialogHeader>
 
             <div class="content">
-                <div
-                    class="flex flex-col gap-4"
-                >
+                <div class="flex flex-col gap-4">
                     <ScrollArea
                         class="h-96 space-y-4 my-4 p-4 bg-white/100 rounded-sm border border-[#E5E7EB] shadow-sm"
                     >
-                        <Table>
-                            <TableHeader>
-                                <TableRow class="[&_th]:px-3 [&_th]:bg-white">
-                                    <TableHead class="!bg-[#ECECEC] w-[140px] z-50 relative">
-                                        <Label>AÑO</Label>
-                                    </TableHead>
-                                    <TableHead>
-                                        <Label>IPC (%)</Label>
-                                    </TableHead>
-                                    <TableHead>
-                                        <Label>HPA (%)</Label>
-                                    </TableHead>
-                                </TableRow>
-                            </TableHeader>
-                            <TableBody>
-                                <TableRow
-                                    class="[&_td]:px-3 [&_td]:bg-white !border-t border-[#ECECEC]"
-                                    v-for="data in localMasterData.macro"
-                                    :key="data.ano"
-                                >
-                                    <TableCell class="!bg-[#ECECEC] font-bold w-[140px]">
-                                        {{ data.ano }}
-                                    </TableCell>
-                                    <TableCell>
-                                        <input
-                                            v-model="data.IPC"
-                                            class="w-full h-full outline-none hover:bg-gray-100"
-                                        >
-                                    </TableCell>
-                                    <TableCell>
-                                        <input
-                                            v-model="data.HPA"
-                                            class="w-full h-full outline-none"
-                                        >
-                                    </TableCell>
-                                </TableRow>
-                            </TableBody>
-                        </Table>
+                        <Tabs default-value="macro">
+                            <TabsList class="grid grid-cols-2">
+                                <TabsTrigger value="macro">
+                                    Macro
+                                </TabsTrigger>
+                                <TabsTrigger value="brokerFee">
+                                    Broker Fee
+                                </TabsTrigger>
+                            </TabsList>
 
-                        <div
-                            v-if="localMasterData.brokerFee?.gestion"
-                            class="mt-6"
-                        >
-                            <Label>Broker Fee - Gestión</Label>
-                            <Table class="mt-2">
-                                <TableHeader>
-                                    <TableRow class="[&_th]:px-3 [&_th]:bg-white">
-                                        <TableHead class="!bg-[#ECECEC] z-50 relative">
-                                            Tramo
-                                        </TableHead>
-                                        <TableHead>Fee (%)</TableHead>
-                                        <TableHead>Cap</TableHead>
-                                        <TableHead>Hurdle</TableHead>
-                                    </TableRow>
-                                </TableHeader>
-                                <TableBody>
-                                    <TableRow
-                                        class="[&_td]:px-3 [&_td]:bg-white !border-t border-[#ECECEC]"
-                                        v-for="data in localMasterData.brokerFee.gestion"
-                                        :key="data.tramo"
-                                    >
-                                        <TableCell class="!bg-[#ECECEC] font-bold">
-                                            {{ data.tramo }}
-                                        </TableCell>
-                                        <TableCell>
-                                            <input
-                                                v-model.number="data.fee"
-                                                class="w-full h-full outline-none hover:bg-gray-100"
+                            <TabsContent value="macro">
+                                <Table>
+                                    <TableHeader>
+                                        <TableRow class="[&_th]:px-3 [&_th]:bg-white">
+                                            <TableHead class="!bg-[#ECECEC] w-[140px] z-50 relative">
+                                                <Label>AÑO</Label>
+                                            </TableHead>
+                                            <TableHead>
+                                                <Label>IPC (%)</Label>
+                                            </TableHead>
+                                            <TableHead>
+                                                <Label>HPA (%)</Label>
+                                            </TableHead>
+                                        </TableRow>
+                                    </TableHeader>
+                                    <TableBody>
+                                        <TableRow
+                                            class="[&_td]:px-3 [&_td]:bg-white !border-t border-[#ECECEC]"
+                                            v-for="data in localMasterData.macro"
+                                            :key="data.ano"
+                                        >
+                                            <TableCell class="!bg-[#ECECEC] font-bold w-[140px]">
+                                                {{ data.ano }}
+                                            </TableCell>
+                                            <TableCell>
+                                                <input
+                                                    v-model="data.IPC"
+                                                    class="w-full h-full outline-none hover:bg-gray-100"
+                                                >
+                                            </TableCell>
+                                            <TableCell>
+                                                <input
+                                                    v-model="data.HPA"
+                                                    class="w-full h-full outline-none"
+                                                >
+                                            </TableCell>
+                                        </TableRow>
+                                    </TableBody>
+                                </Table>
+                            </TabsContent>
+
+                            <TabsContent value="brokerFee">
+                                <div
+                                    v-if="localMasterData.brokerFee?.gestion"
+                                    class="mt-6"
+                                >
+                                    <Table class="mt-2">
+                                        <TableHeader>
+                                            <TableRow class="[&_th]:px-3 [&_th]:bg-white">
+                                                <TableHead class="!bg-[#ECECEC] z-50 relative">
+                                                    Tramo
+                                                </TableHead>
+                                                <TableHead>Fee (%)</TableHead>
+                                                <TableHead>Cap</TableHead>
+                                                <TableHead>Hurdle</TableHead>
+                                            </TableRow>
+                                        </TableHeader>
+                                        <TableBody>
+                                            <TableRow
+                                                class="[&_td]:px-3 [&_td]:bg-white !border-t border-[#ECECEC]"
+                                                v-for="data in localMasterData.brokerFee.gestion"
+                                                :key="data.tramo"
                                             >
-                                        </TableCell>
-                                        <TableCell>
-                                            <input
-                                                v-model.number="data.cap"
-                                                class="w-full h-full outline-none hover:bg-gray-100"
-                                            >
-                                        </TableCell>
-                                        <TableCell>
-                                            <input
-                                                v-model.number="data.hurdle"
-                                                class="w-full h-full outline-none hover:bg-gray-100"
-                                            >
-                                        </TableCell>
-                                    </TableRow>
-                                </TableBody>
-                            </Table>
-                        </div>
+                                                <TableCell class="!bg-[#ECECEC] font-bold">
+                                                    {{ data.tramo }}
+                                                </TableCell>
+                                                <TableCell>
+                                                    <input
+                                                        v-model.number="data.fee"
+                                                        class="w-full h-full outline-none hover:bg-gray-100"
+                                                    >
+                                                </TableCell>
+                                                <TableCell>
+                                                    <input
+                                                        v-model.number="data.cap"
+                                                        class="w-full h-full outline-none hover:bg-gray-100"
+                                                    >
+                                                </TableCell>
+                                                <TableCell>
+                                                    <input
+                                                        v-model.number="data.hurdle"
+                                                        class="w-full h-full outline-none hover:bg-gray-100"
+                                                    >
+                                                </TableCell>
+                                            </TableRow>
+                                        </TableBody>
+                                    </Table>
+                                </div>
+                                <div v-else>
+                                    <!-- ...optional no data message... -->
+                                </div>
+                            </TabsContent>
+                        </Tabs>
                     </ScrollArea>
 
                     <div class="space-y-2">
